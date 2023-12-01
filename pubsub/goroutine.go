650a0f32b4f7234a1d215479d18e69519c9c2d33
Postcount CI <postcount-ci@firegroup.io>
2023-12-01 16:03:30 +0700

    WIP:

--- pubsub/goroutine.go
+++ pubsub/goroutine.go
@@ -11,40 +11,39 @@ import (
 var GoroutineWireset = wire.NewSet(
 	NewGoroutinePublisher,
 	NewGoroutineSubscriber,
+	NewGoChannel,
 )
 
-func NewGoroutineSubscriber(logger *zap.Logger) (message.Subscriber, func(), error) {
-	pubSub := gochannel.NewGoChannel(
-		gochannel.Config{},
-		watermillzap.NewLogger(logger.Named("subscriber")),
-	)
+// for golang, we can use the same channel for both publisher and subscriber
 
+func NewGoChannel(logger *zap.Logger) (*gochannel.GoChannel, func(), error) {
+	channel := gochannel.NewGoChannel(
+		gochannel.Config{},
+		watermillzap.NewLogger(logger.Named("channel")),
+	)
 	cleanup := func() {
-		logger.Info("Router: Cleaning up")
-		if err := pubSub.Close(); err != nil {
+		if err := channel.Close(); err != nil {
 			logger.Error("Router: error closing router", zap.Error(err))
 			return
 		}
 		logger.Info("Router: router closed")
 	}
 
-	return pubSub, cleanup, nil
+	return channel, cleanup, nil
 }
 
-func NewGoroutinePublisher(logger *zap.Logger) (message.Publisher, func(), error) {
-	pubSub := gochannel.NewGoChannel(
-		gochannel.Config{},
-		watermillzap.NewLogger(logger.Named("publisher")),
-	)
+func NewGoroutineSubscriber(logger *zap.Logger, channel *gochannel.GoChannel) (message.Subscriber, func(), error) {
+	cleanup := func() {
+		logger.Info("Router: Cleaning up")
+	}
+	return channel, cleanup, nil
+}
+
+func NewGoroutinePublisher(logger *zap.Logger, channel *gochannel.GoChannel) (message.Publisher, func(), error) {
 
 	cleanup := func() {
 		logger.Info("Router: Cleaning up")
-		if err := pubSub.Close(); err != nil {
-			logger.Error("Router: error closing router", zap.Error(err))
-			return
-		}
-		logger.Info("Router: router closed")
 	}
 
-	return pubSub, cleanup, nil
+	return channel, cleanup, nil
 }
